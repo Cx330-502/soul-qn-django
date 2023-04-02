82d8d713ca4d6dacdefaac2e771af8e77781eac0
“cx330â? <1175963382@qq.com>
2023-04-02 14:29:13 +0800

    瞎写了一堆

--- soul_qn_django/user_about/urls.py
+++ soul_qn_django/user_about/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
 ]
--- soul_qn_django/user_about/views.py
+++ soul_qn_django/user_about/views.py
@@ -1,3 +1,9 @@
+import re
+
+from Qn.models import *
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
