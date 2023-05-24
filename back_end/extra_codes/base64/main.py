418f4a3a4cee001d855ca193e430cffdc769369e
cx330-502 <1175963382@qq.com>
2023-05-24 14:46:32 +0800

    不能用中文......

---
+++ back_end/extra_codes/base64/main.py
@@ -0,0 +1,36 @@
+@@ -1,35 +0,0 @@
+import base64
+import os
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    save_path = "./files.md"
+    file_path = "./files/"
+    with open(save_path, 'w') as save_file:
+        save_file.write('文件列表\n')
+    with open(save_path, 'a') as save_file:
+        save_file.write('\n\n')
+        for files in os.listdir(file_path):
+            file_path1 = os.path.join(file_path, files)
+            with open(file_path1, 'rb') as f:
+                file_content = f.read()
+                file_name = f.name
+            save_file.write('### ' + file_name + '\n')
+            temp = base64.b64encode(file_content).decode('utf-8')
+            save_file.write('##### UTF8码：\n')
+            save_file.write('```' + '\n')
+            save_file.write(temp + '\n')
+            save_file.write('```' + '\n')
+            # save_file.write('##### 二进制码：\n')
+            # save_file.write('```' + '\n')
+            # save_file.write(str(file_content) + '\n')
+            # save_file.write('```' + '\n')
+            save_file.write('##### 解码：\n')
+            save_file.write('```' + '\n')
+            try:
+                save_file.write(base64.b64decode(temp).decode('utf-8') + '\n')
+
+            except:
+                save_file.write('解码失败~\n')
+            save_file.write('```' + '\n')
+            save_file.write('------\n')
